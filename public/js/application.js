4eff7a6ee0b3202b5b47d74c3623005b6b9f1b29
bounceflick <scullingnull@gmail.com>
2015-03-06 13:30:42 -0800

    fuzzy search functionality

--- public/js/application.js
+++ public/js/application.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
   'use strict';
 
   $('#search').on('keyup', function() {
-    var searchtext = $(this).val();
+    // var searchtext = $(this).val();
+    var fuzzySearchText = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$';
+    var reg = RegExp(fuzzySearchText, 'i');
+    var happy;
+    // console.log(fuzzySearchText);
     // console.log($(this).val());
     $(".survey_list a").parents().show();
-    $(".survey_list a").filter(function(item) {return !~$(this).text().indexOf(searchtext)}).parent().hide();
+    // $(".survey_list a").filter(function(item) {return !~$(this).text().indexOf(searchtext)}).parent().hide();
+    $(".survey_list a").filter(function() {
+      happy = $(this).text().replace(/\s+/g, ' ');
+      return !reg.test(happy);}).parent().hide();
     // $(".survey_list a").filter(function(item) {console.log($(this).text().indexOf(searchtext))}).parent().hide();
   });
 });
