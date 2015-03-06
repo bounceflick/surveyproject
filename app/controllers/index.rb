cfb90a13639e37b4f174ae491b29f0706166782d
Lukas Jones & Ridou & Yumiko Huang <pair+lukasjones.ridou.yumikohey@devbootcamp.com>
2015-03-06 13:56:06 -0800

    create survey without the complete button

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,48 @@
+['/', '/survey/new', '/survey/create'].each do |route|
+  before route do
+    @user = User.find(session[:id])
+  end
+end
+
 get '/' do
-  erb :index
+  if session[:id]
+   # user = User.find(session[:id])
+    redirect "/user/#{@user.id}"
+  else
+    erb :index
+  end
 end
 
 get '/user/:id' do
   @all_surveys = Survey.all
   erb :profile
-end 
-
-get '/surveys/:id' do 
-  "You got to "
 end
 
+# get '/survey/new' do
+#   @user = User.find(session[:id])
+# end
+
+post '/survey/create' do
+ # @user = User.find(session[:id])
+  survey = @user.surveys.create(title: params[:title])
+  redirect "/surveys/#{survey.id}"
+end
+
+get '/surveys/:id' do
+  @survey = Survey.find(params[:id])
+  @questions = Question.where(id: @survey.id)
+  erb :"survey/create"
+end
+
+post '/surveys/:id/question/create' do
+  puts "PARAMS id"
+  puts params[:description]
+  @survey = Survey.find(params[:id])
+  puts "SURVEY"
+  p @survey
+  question = @survey.questions.create(description: params[:description])
+  puts "QUESTION"
+  p question
+
+  erb :"survey/_question", locals: {question: question}, layout: false
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  // event handler
+  // prevent default
+  // go to this ajax function
+  $("body").on("submit", ".create_question", function(event){
+    event.preventDefault();
+    createQuestion();
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+
+
+
+
+  createQuestion = function () {
+    var route = $('.add-question').attr('action')
+    var formData = $('.add-question').serialize()
+    $.ajax({
+      url: route,
+      type: 'POST',
+      data: formData,
+    })
+    .done(function(response) {
+      $('.questionsList').append(response)
+      $('#questions').replaceWith(formData.description)
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  };
+
+
 });
