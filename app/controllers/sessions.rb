edc966beca267bc256ab3ff5240e76cad22e1209
Lukas Jones & Yumiko Huang <pair+lukasjones.yumikohey@devbootcamp.com>
2015-03-05 17:07:45 -0800

    added b-crypt and user validation

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,18 +1,18 @@
 post '/signin' do
 
-  @user = User.where(email: params[:email])
-  puts @user
-  if !@user.empty?
+  @user = User.where(email: params[:email])[0]
+  p @user
+  if @user && @user.authenticate(params[:password])
     session[:id] = @user.id
     redirect "/" #change to user route
   else
+    puts "You failed!"
     erb :index
   end
 
 end
 
 get '/signup' do
-
   erb :"users/new"
 end
 
